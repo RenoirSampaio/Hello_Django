faf6567f3d1cde998f3a09f3af0e7cecb76e9123
Renoir Sampaio <renoirfla@gmail.com>
2020-06-03 00:25:45 -0300

    New Parameters URL

--- core/views.py
+++ core/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def hello(request, nome, idade):
-  return HttpResponse('<h1>Hello {} de {} anos!</h1>'.format(nome, idade))
+def hello(request, nome, idade, somar):
+  soma = idade + somar
+  return HttpResponse('<h1>Hello {} de {} anos e daqui {} anos tera {} anos!</h1>'.format(nome, idade, somar, soma))
--- hello_django/urls.py
+++ hello_django/urls.py
@@ -19,5 +19,5 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/<int:idade>/' , views.hello)
+    path('hello/<nome>/<int:idade>/<int:somar>/' , views.hello)
 ]
